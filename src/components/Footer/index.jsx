37636afe62dbd2b6e515fe00b0f47003391c96f8
roxannefranco <roxannefranco10@gmail.com>
2023-10-13 20:41:42 +0200

    Added footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from "./styles.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p className="text">
+        All rights reserved <span>©</span> Portfolio 2022
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { darkModeAtom } from "../config/theme";
 import { useAtom } from "jotai";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 function Home() {
   const [darkMode] = useAtom(darkModeAtom);
@@ -14,6 +15,7 @@ function Home() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
